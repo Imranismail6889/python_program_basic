4d55416b6949fd54434961cae3ffce8e365e61e9
Imranismail6889 <imranismail6889@gmail.com>
2026-09-22 15:38:19 +0530

    Refactor factorial calculation and output formatting

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+if number < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    factorial = 1
+    for i in range(1,number + 1):
+        factorial *= i
+    print("Factorial of",number,"is",factorial)
